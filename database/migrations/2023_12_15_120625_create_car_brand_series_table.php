e0448a1dc6bf2e0b3165b44af66fd0517c52fa25
Illusion <1161524405@qq.com>
2023-12-15 20:49:12 +0800

    创建字段的提示

--- database/migrations/2023_12_15_120625_create_car_brand_series_table.php
+++ database/migrations/2023_12_15_120625_create_car_brand_series_table.php
@@ -19,10 +19,14 @@ return new class extends Migration
             $table->string("title")->comment("汽车品牌系列名称");
             $table->string("english_title")->comment("汽车品牌系列英文名称");
             $table->string("dealer_price")->comment("汽车品牌车系经销商价格")->comment("");
+            // ! create a new migration if needed.
             // $table->decimal("dealer_price_min", 10, 2)->comment("经销商价格下限");
+            // ! create a new migration if needed.
             // $table->decimal("dealer_price_max", 10, 2)->comment("经销商价格上限");
             $table->string("offical_price")->comment("汽车品牌车系官方价格")->comment("");
+            // ! create a new migration if needed.
             // $table->decimal("offical_price_min", 10, 2)->comment("官方价格下限");
+            // ! create a new migration if needed.
             // $table->decimal("offical_price_max", 10, 2)->comment("官方价格上限");
             $table->timestamps();
         });
