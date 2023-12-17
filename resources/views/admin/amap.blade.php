b3654a35b00d098b0c1a685748e4281181e96938
Illusion <1161524405@qq.com>
2023-12-17 11:46:17 +0800

    车主信息

--- resources/views/admin/amap.blade.php
+++ resources/views/admin/amap.blade.php
@@ -3,7 +3,7 @@
     <div class="{{$viewClass['field']}}">
         <input hidden id="{{$id['lng']}}" name="{{$id['lng']}}" value="0" />
         <input hidden id="{{$id['lat']}}" name="{{$id['lat']}}" value="0" />
-        <div><input id="tipinput" /></div>
+        <div><input id="tipinput" style="margin-bottom: 10px" placeholder="搜索关键字" /></div>
         <div id="container" style="width: 100%; height: 500px"></div>
     </div>
 </div>
