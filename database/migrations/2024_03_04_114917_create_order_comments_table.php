54035bcc80c791496f1df64d6d1d07aafb70adfb
Illusion <1161524405@qq.com>
2024-03-04 22:22:52 +0800

    订单评价模块

--- database/migrations/2024_03_04_114917_create_order_comments_table.php
+++ database/migrations/2024_03_04_114917_create_order_comments_table.php
@@ -14,8 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('order_comments', function (Blueprint $table) {
+            $defaultMaxRate = 5;
             $table->id();
-            
+            $table->unsignedBigInteger("order_id");
+            $table->unsignedBigInteger("user_id");
+            $table->timestamp("published_at")->nullable();
+            // ? if need the below column?
+            $table->string("title")->nullable();
+            $table->unsignedTinyInteger("rate")->default($defaultMaxRate);
+            $table->string("content")->default("");
+            // 0 => 未知, 1 => 小程序, 2 => App
+            $table->unsignedTinyInteger("client")->default(0);
             $table->timestamps();
         });
     }
